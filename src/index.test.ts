843200477d32c6936d5ddef8c58f8b171fac3a43
Felix Becker <felix@sourcegraph.com>
2019-04-24 22:35:34 +0200

    feat: support configuration (#3)

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,21 +7,39 @@ describe('createStubSourcegraphAPI()', () => {
         const stub = createStubSourcegraphAPI()
         assert(stub)
     })
-    it('should support creating unique decorationTypes', () => {
-        const stub = createStubSourcegraphAPI()
-        assert.deepStrictEqual(stub.app.createDecorationType(), { key: 'decorationType0' })
-        assert.deepStrictEqual(stub.app.createDecorationType(), { key: 'decorationType1' })
-        assert.deepStrictEqual(stub.app.createDecorationType(), { key: 'decorationType2' })
+    describe('app', () => {
+        it('should support creating unique decorationTypes', () => {
+            const stub = createStubSourcegraphAPI()
+            assert.deepStrictEqual(stub.app.createDecorationType(), { key: 'decorationType0' })
+            assert.deepStrictEqual(stub.app.createDecorationType(), { key: 'decorationType1' })
+            assert.deepStrictEqual(stub.app.createDecorationType(), { key: 'decorationType2' })
+        })
     })
-
-    // TODO fix Position/Range classes (publish as package) and MarkupKind enum assignability
-    // it('should allow registering a hover provider', () => {
-    //     const stub = createStubSourcegraphAPI()
-    //     stub.languages.registerHoverProvider(['*.ts'], {
-    //         provideHover: (doc, pos) => ({
-    //             contents: { kind: stub.MarkupKind.Markdown, value: doc.uri },
-    //             range: new stub.Range(new stub.Position(1, 2), new stubs.Position(3, 4)),
-    //         }),
+    describe('configuration', () => {
+        it('should support reading configuration', async () => {
+            const stub = createStubSourcegraphAPI()
+            assert.deepStrictEqual(stub.configuration.get().value, {})
+            const listener = sinon.spy()
+            stub.configuration.subscribe(listener)
+            sinon.assert.calledOnce(listener)
+            assert.deepStrictEqual(listener.args[0][0], undefined)
+            await stub.configuration.get().update('foo', 'bar')
+            assert.deepStrictEqual(stub.configuration.get().value, { foo: 'bar' })
+            assert.deepStrictEqual(stub.configuration.get().get('foo'), 'bar')
+            sinon.assert.calledTwice(listener)
+            assert.deepStrictEqual(listener.args[1][0], undefined)
+        })
+    })
+    // describe('languages', () => {
+    //     // TODO fix Position/Range classes (publish as package) and MarkupKind enum assignability
+    //     it('should allow registering a hover provider', () => {
+    //         const stub = createStubSourcegraphAPI()
+    //         stub.languages.registerHoverProvider(['*.ts'], {
+    //             provideHover: (doc, pos) => ({
+    //                 contents: { kind: stub.MarkupKind.Markdown, value: doc.uri },
+    //                 range: new stub.Range(new stub.Position(1, 2), new stubs.Position(3, 4)),
+    //             }),
+    //         })
     //     })
     // })
 })
